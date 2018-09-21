28bf25d079964e24f091159dd3a025bd6ddaadc1
Jessie <jessie.wang@hotmail.co.uk>
2018-09-21 12:14:47 +1000

    completed terminal questions

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log('checkpoint')
+
+
+/*
+#### Assume your present working directory is `$ ~/buffy`
+
+1. Make two directories inside `~/buffy`: `scoobies` and `vamps`
+
+pwd
+mkdir scoobies vamps
+
+2. Make files in `scoobies` named `buffy.txt`, `giles.txt` and `angel.txt`
+
+cd scoobies 
+touch buffy.txt giles.txt angle.txt
+
+3. Copy `angel.txt` into the `vamps` directory
+
+pwd // assume I'm in scoobies directory
+cp angle.txt ~/wdi17/checkpoint/checkpoint1/vamps/
+
+
+4. Delete the `vamps` directory and everything inside it
+
+cd ..
+rm -rf vamps 
+*/
+
+
+// JS Variables
+
+1. 
+
+var captain;
+
+captain = 'Jack'
+
+2. 
+
+var phrase = 'Oh ' + captain + ', my ' + captain + "!"
+
+// JS conditionals 
+
+
